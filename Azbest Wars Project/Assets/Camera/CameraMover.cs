a64bc363f1920dfb7467bc347bbc8787d9005e19
Jczelu1 <julian2008c@interia.pl>
2025-03-15 23:15:54 +0100

    better camera movement

--- Azbest Wars Project/Assets/Camera/CameraMover.cs
+++ Azbest Wars Project/Assets/Camera/CameraMover.cs
@@ -2,14 +2,51 @@ using UnityEngine;
 
 public class CameraMover : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    // Time interval between moves in seconds
+    public float moveInterval = 0.25f;
+    private float timer = 0f;
+    private int horizontal = 0;
+    private int vertical = 0;
+    private bool moving = false;
 
     void Update()
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");   
+        timer += Time.deltaTime;
+        int newHorizontal = (int)Input.GetAxisRaw("Horizontal");
+        int newVertical = (int)Input.GetAxisRaw("Vertical");
+        if (!moving)
+        {
+            if (newHorizontal != 0)
+            {
+                horizontal = newHorizontal;
+            }
+            if (newVertical != 0)
+            {
+                vertical = newVertical;
+            }
+        }
+        if (timer >= moveInterval)
+        {
+            if (moving)
+            {
+                horizontal = newHorizontal;
+                vertical = newVertical;
+            }
+            moving = false;
+            if (horizontal != 0 || vertical != 0)
+            {
+                moving = true;
 
-        Vector3 move = new Vector3(moveX, moveY, 0) * moveSpeed * Time.deltaTime;
-        transform.Translate(move, Space.World);
+                Vector3 move = new Vector3(horizontal, vertical, 0);
+
+                if (move.magnitude > 1)
+                    move.Normalize();
+
+                transform.Translate(move, Space.World);
+                horizontal = 0;
+                vertical = 0;
+            }
+            timer = 0f;
+        }
     }
 }
