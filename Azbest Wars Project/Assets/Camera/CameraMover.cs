ca7e461ddf9832c5a337cf8b8bea7e1dc23d3278
Jczelu1 <julian2008c@interia.pl>
2025-04-03 20:57:04 +0200

    better camera movement

--- Azbest Wars Project/Assets/Camera/CameraMover.cs
+++ Azbest Wars Project/Assets/Camera/CameraMover.cs
@@ -1,52 +1,60 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraMover : MonoBehaviour
 {
-    // Time interval between moves in seconds
     public float moveInterval = 0.25f;
     private float timer = 0f;
-    private int horizontal = 0;
-    private int vertical = 0;
+    private Vector2 movementInput = Vector2.zero;
     private bool moving = false;
+    public Vector2 minCameraPosition;
+    public Vector2 maxCameraPosition;
+
+    private InputAction moveAction;
+
+    void Awake()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        moveAction.Enable();
+    }
 
     void Update()
     {
         timer += Time.deltaTime;
-        int newHorizontal = (int)Input.GetAxisRaw("Horizontal");
-        int newVertical = (int)Input.GetAxisRaw("Vertical");
-        if (!moving)
+
+        Vector2 newInput = moveAction.ReadValue<Vector2>();
+        if (!moving && newInput != Vector2.zero)
         {
-            if (newHorizontal != 0)
-            {
-                horizontal = newHorizontal;
-            }
-            if (newVertical != 0)
-            {
-                vertical = newVertical;
-            }
+            movementInput = newInput;
         }
+
         if (timer >= moveInterval)
         {
+
             if (moving)
             {
-                horizontal = newHorizontal;
-                vertical = newVertical;
+                movementInput = newInput;
             }
             moving = false;
-            if (horizontal != 0 || vertical != 0)
+
+            if (movementInput != Vector2.zero)
             {
                 moving = true;
+                Vector3 move = new Vector3((Mathf.Abs(movementInput.x)>0.1)?Mathf.Sign(movementInput.x):0, (Mathf.Abs(movementInput.y) > 0.1) ? Mathf.Sign(movementInput.y) : 0, 0);
 
-                Vector3 move = new Vector3(horizontal, vertical, 0);
-
-                if (move.magnitude > 1)
-                    move.Normalize();
-
+                float x = transform.position.x + move.x;
+                float y = transform.position.y + move.y;
+                if (x < minCameraPosition.x || x > maxCameraPosition.x) move.x = 0;
+                if (y < minCameraPosition.y || y > maxCameraPosition.y) move.y = 0;
                 transform.Translate(move, Space.World);
-                horizontal = 0;
-                vertical = 0;
+                movementInput = Vector2.zero;
             }
             timer = 0f;
         }
+    }
+
+    private void OnDisable()
+    {
+        moveAction.Disable();
     }
 }
---
+++ Azbest Wars Project/Assets/Input/MainInputScript.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MainInputSystem : MonoBehaviour
+{
+    //public InputAction selectAction;
+
+    //// A static variable or a singleton data structure to share input values with ECS systems
+    //public static Vector2 moveInput;
+
+    //private void Start()
+    //{
+    //    selectAction = InputSystem.actions.FindAction("Attack"); ;
+    //}
+    //private void OnEnable()
+    //{
+    //    selectAction.Enable();
+    //}
+
+    //private void OnDisable()
+    //{
+    //    selectAction.Disable();
+    //}
+
+    //private void Update()
+    //{
+    //    moveInput = selectAction.ReadValue<Vector2>();
+    //}
+}
