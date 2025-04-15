83320cd36284d06fcb2387746206e075eba89ff4
Jczelu1 <julian2008c@interia.pl>
2025-04-15 13:37:09 +0200

    movement state batons

---
+++ Azbest Wars Project/Assets/UI/Scripts/UnitStateInput.cs
@@ -0,0 +1,62 @@
+using System.Linq;
+using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UnitStateInput : MonoBehaviour
+{
+    InputAction DefendAction;
+    InputAction AttackAction;
+    InputAction StopAction;
+
+    [SerializeField]
+    CustomButtonScript[] movementStateButtons;
+
+    public static byte currentMovementState = 255;
+    void Start()
+    {
+        DefendAction = InputSystem.actions.FindAction("Defend");
+        AttackAction = InputSystem.actions.FindAction("Attack");
+        StopAction = InputSystem.actions.FindAction("Stop");
+    }
+
+    void Update()
+    {
+       for (int i = 0; i < 2; i++)
+        {
+            movementStateButtons[i].UnSelect();
+        }
+        if (currentMovementState < 3)
+        {
+            if (currentMovementState == 2) currentMovementState = 0;
+            movementStateButtons[currentMovementState].Select();
+        }
+        if (MainGridScript.Instance.Selected)
+        {
+            if (DefendAction.WasPressedThisFrame())
+            {
+                SetMovementState("0");
+            }
+            if (AttackAction.WasPressedThisFrame())
+            {
+                SetMovementState("1");
+            }
+            if (StopAction.WasPressedThisFrame())
+            {
+                SetMovementState("2");
+            }
+        }
+        
+    }
+    public void SetMovementState(string state)
+    {
+        byte s;
+        if(byte.TryParse(state, out s))
+        {
+            if (s > 2) return;
+            PathfindSystem.setMoveState[TeamManager.Instance.PlayerTeam] = s;
+            currentMovementState = s;
+        }
+    }
+}
