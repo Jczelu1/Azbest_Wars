f0605daa7ed9a0822036e31a5a01e4efb57983b4
Jczelu1 <julian2008c@interia.pl>
2025-04-23 10:11:08 +0200

    there is nothing we can do win condition

--- Azbest Wars Project/Assets/Other/WinConditionSystem.cs
+++ Azbest Wars Project/Assets/Other/WinConditionSystem.cs
@@ -15,12 +15,18 @@ public partial class WinConditionSystem : SystemBase
     public static bool EndIfCompleted = false;
     public static bool Ended = false;
     public static bool Win = false;
+    private int startDelay = 10;
     protected override void OnCreate()
     {
         RequireForUpdate<CaptureAreaData>();
     }
     protected override void OnUpdate()
     {
+        if(startDelay > 0)
+        {
+            startDelay--;
+            return;
+        }
         if (TimeLeftSeconds == -1) return;
         TimeLeftSeconds -= TickSystemGroup.TimePerTick;
         if (TotalWinAreas == -1)
@@ -34,13 +40,52 @@ public partial class WinConditionSystem : SystemBase
             }).Run();
         }
         CapturedWinAreas = 0;
-        Entities.WithoutBurst().ForEach((ref CaptureAreaData captureArea, ref TeamData team) =>
+        bool PlayerHasSpawner = false;
+        bool EnemyHasSpawner = false;
+        bool PlayerHasUnits = false;
+        bool EnemyHasUnits = false;
+        Entities.WithoutBurst().ForEach((in CaptureAreaData captureArea, in TeamData team) =>
         {
+            if (captureArea.HasSpawner)
+            {
+                if(team.Team == TeamManager.Instance.PlayerTeam)
+                {
+                    PlayerHasSpawner = true;
+                }
+                else if(team.Team == TeamManager.Instance.AITeam)
+                {
+                    EnemyHasSpawner = true;
+                }
+            }
             if (!captureArea.WinCondition) return;
             if (team.Team != TeamManager.Instance.PlayerTeam) return;
             CapturedWinAreas++;
         }).Run();
-        if(EndIfCompleted && CapturedWinAreas == RequiredWinAreas)
+        Entities.WithoutBurst().ForEach((in UnitStateData unitState, in TeamData team) =>
+        {
+            if(!PlayerHasUnits && team.Team == TeamManager.Instance.PlayerTeam)
+            {
+                PlayerHasUnits = true;
+            }
+            if(!EnemyHasUnits && team.Team == TeamManager.Instance.AITeam)
+            {
+                EnemyHasUnits = true;
+            }
+        }).Run();
+        if (!EnemyHasUnits && !EnemyHasSpawner)
+        {
+            Ended = true;
+            Win = true;
+            return;
+        }
+        else if (!PlayerHasUnits && !PlayerHasSpawner)
+        {
+            Ended = true;
+            Win = false;
+            return;
+        }
+
+        if (EndIfCompleted && CapturedWinAreas == RequiredWinAreas)
         {
             Ended = true;
             Win = true;
